1f53d24a6eb87e20f92972e6d00f0349d1bd530b
Prieyudha Akadita S <akaditasustono@gmail.com>
2021-07-06 00:19:09 +0700

    fix cannot upload image

--- app/src/main/java/syaiful/kirom/pengaduanbanjaranyar/activities/CreateAduanActivity.kt
+++ app/src/main/java/syaiful/kirom/pengaduanbanjaranyar/activities/CreateAduanActivity.kt
@@ -1,23 +1,32 @@
 package syaiful.kirom.pengaduanbanjaranyar.activities
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.FileUtils
+import android.util.Log
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import coil.load
+import com.esafirm.imagepicker.features.ImagePickerConfig
+import com.esafirm.imagepicker.features.ImagePickerMode
+import com.esafirm.imagepicker.features.createImagePickerIntent
+import com.esafirm.imagepicker.features.registerImagePicker
+import com.esafirm.imagepicker.model.Image
 import kotlinx.android.synthetic.main.activity_create_aduan.*
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import syaiful.kirom.pengaduanbanjaranyar.R
 import syaiful.kirom.pengaduanbanjaranyar.contracts.InformasiActivityContracts
 import syaiful.kirom.pengaduanbanjaranyar.databinding.ActivityCreateAduanBinding
 import syaiful.kirom.pengaduanbanjaranyar.models.Category
 import syaiful.kirom.pengaduanbanjaranyar.presenters.InformasiActivityCreatePresenter
 import syaiful.kirom.pengaduanbanjaranyar.utilities.Constants
 import java.io.File
+
 
 @Suppress("DEPRECATION")
 class CreateAduanActivity : AppCompatActivity(), InformasiActivityContracts.InformasiActivityCreateView,
@@ -27,6 +36,15 @@ class CreateAduanActivity : AppCompatActivity(), InformasiActivityContracts.Info
     private lateinit var binding : ActivityCreateAduanBinding
     private lateinit var imageUri : Uri
 
+
+    private var choosedImage: Image? = null
+
+    private val imagePickerLauncher = registerImagePicker {
+        choosedImage = it[0]
+        showImage()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -35,8 +53,10 @@ class CreateAduanActivity : AppCompatActivity(), InformasiActivityContracts.Info
         setContentView(binding.root)
 
         binding.imageView.setOnClickListener {
-            openGalery()
+//            openGalery()
+            chooseImage()
         }
+
         doSave()
         toolbarAction()
     }
@@ -46,19 +66,34 @@ class CreateAduanActivity : AppCompatActivity(), InformasiActivityContracts.Info
         presenter?.getCategory(token)
     }
 
+    private fun showAlertDialog(message: String){
+        AlertDialog.Builder(this@CreateAduanActivity).apply {
+            setMessage(message)
+            setPositiveButton("OK"){ d, _ ->
+                d.cancel()
+            }
+        }.show()
+    }
+
     private fun doSave() {
         binding.btnSave.setOnClickListener {
+            if(choosedImage == null){
+                showAlertDialog(getString(R.string.validation_no_image))
+                return@setOnClickListener
+            }
+
+
             val token = Constants.getToken(this)
             val selectedObject = binding.spinnerCategory.selectedItem as Category
             val complaintContent : RequestBody = RequestBody.create(
                 MultipartBody.FORM, binding.etComplaintContent.text.toString()
-            );
-            val complaintCategory : RequestBody = RequestBody.create(MultipartBody.FORM, selectedObject.id);
+            )
+            val complaintCategory : RequestBody = RequestBody.create(MultipartBody.FORM, selectedObject.id)
 
-            val originalFile  = File(imageUri.path)
+            val originalFile = File(choosedImage?.path!!)
 
             val imagePart : RequestBody = RequestBody.create(
-                MediaType.parse(contentResolver.getType(imageUri)),
+                MediaType.parse("image/*"),
                 originalFile
             )
 
@@ -68,8 +103,25 @@ class CreateAduanActivity : AppCompatActivity(), InformasiActivityContracts.Info
                 imagePart
             )
 
-            presenter?.postComplaint(token, complaintCategory, complaintContent, image)
+            presenter?.postComplaint("Bearer $token", complaintCategory, complaintContent, image)
         }
+    }
+
+    private fun chooseImage(){
+        val config = ImagePickerConfig{
+            mode = ImagePickerMode.SINGLE
+            isIncludeVideo = false
+            isShowCamera = false
+        }
+
+        imagePickerLauncher.launch(config)
+
+    }
+
+    private fun showImage(){
+        choosedImage?.let { image ->
+            binding.imageView.load(image.uri)
+        } ?: binding.imageView.load(R.drawable.no_image)
     }
 
     private fun openGalery(){
@@ -85,6 +137,7 @@ class CreateAduanActivity : AppCompatActivity(), InformasiActivityContracts.Info
             binding.imageView.setImageURI(imageUri)
         }
     }
+
 
     private fun toolbarAction(){
         val action = supportActionBar
